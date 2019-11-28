117f42ae569e5e81b8fbf31bf6fe328ceba38b02
Michael Herman <hermanmu@gmail.com>
2019-11-27 23:33:03 -0700

    rebase

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -8,7 +8,7 @@ class Calculator extends Component {
     // value to be displayed in <Display />
     displayValue: '0',
     // values to be displayed in number <Keys />
-    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
+    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0', 'ce'],
     // values to be displayed in operator <Keys />
     operators: ['/', 'x', '-', '+'],
     // operator selected for math operation
