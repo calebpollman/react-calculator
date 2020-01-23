bcc501bcd8ec632cbf9b0c748956b1d0cb6ef98d
evelyn.graumann <evelyn.graumann@crosslend.com>
2020-01-23 13:23:21 +0100

    react tutorial: finish functionality from layout

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -18,15 +18,81 @@ class Calculator extends Component {
     }
 
     callOperator = () => {
-        console.log('call operation');
+        let { displayValue, selectedOperator, storedValue } = this.state;
+        // temp variable for updating state storedValue
+        const updateStoredValue = displayValue;
+
+        // parse strings for operations
+        displayValue = parseInt(displayValue, 10);
+        storedValue = parseInt(storedValue, 10);
+
+        // performs selected operation
+        switch (selectedOperator) {
+            case '+':
+                displayValue = storedValue + displayValue;
+                break;
+            case '-':
+                displayValue = storedValue - displayValue;
+                break;
+            case 'x':
+                displayValue = storedValue * displayValue;
+                break;
+            case '/':
+                displayValue = storedValue / displayValue;
+                break;
+            default:
+                // set displayValue to zero if no case matches
+                displayValue = '0';
+        }
+
+        // converts displayValue to a string
+        displayValue = displayValue.toString();
+        // reset selectedOperator
+        selectedOperator = '';
+        // check for 'NaN' or 'Infinity', if true set displayValue to '0'
+        if (displayValue === 'NaN' || displayValue === 'Infinity') displayValue = '0';
+
+        this.setState({ displayValue, selectedOperator, storedValue: updateStoredValue });
     }
 
-    setOperator = () => {
-        console.log('set operation');
+    setOperator = value => {
+        let { displayValue, selectedOperator, storedValue } = this.state;
+
+        // check if a value is already present for selectedOperator
+        if (selectedOperator === '') {
+            // update storedValue to the value of displayValue
+            storedValue = displayValue;
+            // reset the value of displayValue to '0'
+            displayValue = '0';
+            // update the value of selectedOperator to the given value
+            selectedOperator = value;
+        } else {
+            // if selectedOperator is not an empty string
+            // update the value of selectedOperator to the given value
+            selectedOperator = value;
+        }
+
+        this.setState({ displayValue, selectedOperator, storedValue });
     }
 
-    updateDisplay = () => {
-        console.log('update display');
+    updateDisplay = value => {
+        let { displayValue } = this.state;
+
+        // prevent multiple occurences of '.'
+        if (value === '.' && displayValue.includes('.')) value = '';
+
+        if (value === 'ce') {
+            // deletes last char in displayValue
+            displayValue = displayValue.substr(0, displayValue.length - 1);
+            // set displayValue to '0' if displayValue is empty string
+            if (displayValue === '') displayValue = '0';
+        } else {
+            // replace displayValue with value if displayValue equal to '0'
+            // else concatenate displayValue and value
+            displayValue === '0' ? displayValue = value : displayValue += value;
+        }
+
+        this.setState({ displayValue });
     }
 
     render = () => {
--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import './Key.css';
 
 const Key = ({ keyAction, keyType, keyValue }) => (
-    <div className={`key-container ${keyType}`}>
+    <div
+        className={`key-container ${keyType}`}
+        onClick={() => keyAction(keyValue)}
+    >
         <p className="key-value">
         {keyValue}
         </p>
